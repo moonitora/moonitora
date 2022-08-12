0791e3e4a46c82c4523cf9bbb06629c8f8e80351
Victor Betoni <victorbetoni@protonmail.com>
2022-08-12 07:56:01 -0300

    Corrigindo tipos

--- model/horario_model.go
+++ model/horario_model.go
@@ -2,9 +2,9 @@ package model
 
 type Horario struct {
 	Monitor        string `json:"monitor" db:"monitor"`
-	DiaDaSemana    string `json:"dia_da_semana" db:"dia_da_semana"`
-	InicioHoras    string `json:"inicio_horas" db:"inicio_horas"`
-	InicioMinutos  string `json:"inicio_minutos" db:"inicio_minutos"`
-	TerminoHoras   string `json:"termino_horas" db:"termino_horas"`
-	TerminoMinutos string `json:"termino_minutos" db:"termino_minutos"`
+	DiaDaSemana    int    `json:"dia_da_semana" db:"dia_da_semana"`
+	InicioHoras    int    `json:"inicio_horas" db:"inicio_horas"`
+	InicioMinutos  int    `json:"inicio_minutos" db:"inicio_minutos"`
+	TerminoHoras   int    `json:"termino_horas" db:"termino_horas"`
+	TerminoMinutos int    `json:"termino_minutos" db:"termino_minutos"`
 }
