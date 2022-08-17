1b10efeb7d2f92dbf77606192e4bc2a31af89cb0
Victor Betoni <victorbetoni@protonmail.com>
2022-08-17 18:43:38 -0300

    Mais debug

--- controller/monitorias_controller.go
+++ controller/monitorias_controller.go
@@ -62,7 +62,8 @@ func PostMonitoria(c *gin.Context) (int, error) {
 
 	db := database.GrabDB()
 	var foo model.Monitoria
-	if err := db.Get(&foo, "SELECT * FROM monitorias WHERE horario=$1 AND monitor=$2 AND data=$3", monitoria.Horario, monitoria.Monitor, monitoria.Data); err == nil || (err != nil && err != sql.ErrNoRows) {
+	fmt.Println(monitoria.Horario, " ", monitoria.Data)
+	if err := db.Get(&foo, "SELECT * FROM monitorias WHERE horario=$1 AND data=$3", monitoria.Horario, monitoria.Monitor, monitoria.Data); err == nil || (err != nil && err != sql.ErrNoRows) {
 		return http.StatusBadRequest, errors.New("Dia e horario do monitor ja ocupado")
 	}
 
