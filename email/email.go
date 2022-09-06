1714604e0acdd2c94dbca6d8d3e54387ee47a039
Victor Betoni <victorbetoni@protonmail.com>
2022-09-06 17:13:30 -0300

    Debug

--- email/email.go
+++ email/email.go
@@ -1,6 +1,7 @@
 package email
 
 import (
+	"fmt"
 	"github.com/victorbetoni/moonitora/model"
 	"gopkg.in/gomail.v2"
 	"os"
@@ -8,13 +9,24 @@ import (
 )
 
 func NotifyMonitor(monitor string, monitoria model.Monitoria) error {
+
+	email := strings.TrimSpace(os.Getenv("APP_EMAIL"))
+	user := strings.TrimSpace(os.Getenv("APP_EMAIL_USER"))
+	pw := strings.TrimSpace(os.Getenv("APP_EMAIL_PASSWORD"))
+	mn := strings.TrimSpace(monitor)
+
+	fmt.Println("From:", email)
+	fmt.Println("User:", user)
+	fmt.Println("Pw:", pw)
+	fmt.Println("Monitor:", mn)
+
 	msg := gomail.NewMessage()
-	msg.SetHeader("From", strings.TrimSpace(os.Getenv("APP_EMAIL")))
-	msg.SetHeader("To", monitor)
+	msg.SetHeader("From", email)
+	msg.SetHeader("To", mn)
 	msg.SetHeader("Subject", "Monitoria marcada para você")
 	msg.SetBody("text/html", "<b>Uma nova monitoria foi marcada para você</b>")
 
-	n := gomail.NewDialer("smtp.gmail.com", 587, strings.TrimSpace(os.Getenv("APP_EMAIL_USER")), strings.TrimSpace(os.Getenv("APP_EMAIL_PASSWORD")))
+	n := gomail.NewDialer("smtp.gmail.com", 587, user, pw)
 
 	if err := n.DialAndSend(msg); err != nil {
 		return err
