9469290101bd61760caba142a89f6549b7ff5c21
Victor Betoni <victorbetoni@protonmail.com>
2022-09-06 17:07:39 -0300

    Fazendo trim nas variaveis de ambiente

--- email/email.go
+++ email/email.go
@@ -1,25 +1,20 @@
 package email
 
 import (
-	"fmt"
 	"github.com/victorbetoni/moonitora/model"
 	"gopkg.in/gomail.v2"
 	"os"
+	"strings"
 )
 
 func NotifyMonitor(monitor string, monitoria model.Monitoria) error {
 	msg := gomail.NewMessage()
-	msg.SetHeader("From", os.Getenv("APP_EMAIL"))
+	msg.SetHeader("From", strings.TrimSpace(os.Getenv("APP_EMAIL")))
 	msg.SetHeader("To", monitor)
 	msg.SetHeader("Subject", "Monitoria marcada para você")
 	msg.SetBody("text/html", "<b>Uma nova monitoria foi marcada para você</b>")
 
-	fmt.Println("From: ", os.Getenv("APP_EMAIL"))
-	fmt.Println("Monitor:", monitor)
-	fmt.Println("user: ", os.Getenv("APP_EMAIL_USER"))
-	fmt.Println("password: ", os.Getenv("APP_EMAIL_PASSWORD"))
-
-	n := gomail.NewDialer("smtp.gmail.com", 587, os.Getenv("APP_EMAIL_USER"), os.Getenv("APP_EMAIL_PASSWORD"))
+	n := gomail.NewDialer("smtp.gmail.com", 587, strings.TrimSpace(os.Getenv("APP_EMAIL_USER")), strings.TrimSpace(os.Getenv("APP_EMAIL_PASSWORD")))
 
 	if err := n.DialAndSend(msg); err != nil {
 		return err
