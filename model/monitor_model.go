667107c0071fe191f28a81df5e7806aa19540d05
Victor Betoni <victorbetoni@protonmail.com>
2022-09-19 13:44:10 -0300

    Adicionando campo de ativado

--- model/monitor_model.go
+++ model/monitor_model.go
@@ -5,6 +5,7 @@ type Monitor struct {
 	RA           string `json:"ra" db:"ra"`
 	Email        string `json:"email" db:"email"`
 	Departamento string `json:"departamento" db:"departamento"`
+	Ativado      int    `json:"ativado" db:"ativado"`
 	Adm          int    `json:"adm" db:"adm"`
 }
 
